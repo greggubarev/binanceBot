efdd08cbf446f99923e5ddba7612c4d89c6e740a
Григорий Губарев <g.gubarev@outlook.com>
2021-03-20 02:59:19 +0400

    init4

--- main.js
+++ main.js
@@ -2,8 +2,14 @@
     let url = 'https://api.binance.com/wapi/v3/systemStatus.html';
     let response = await fetch(url);
     let state = await response.json(); // читаем ответ в формате JSON
-    // let statusServ = document.getElementsByClassName("statusService");
+    let statusServ = document.getElementsByClassName("statusService1");
     // return statusServ.textContent += state.status;
+    if (state.status == "0") {
+        statusServ = "online";
+    } else {
+        statusServ = "maintenance";
+
+    }
         return console.log("state is " + state.msg + " status is " + state.status);
     //
     // let statusServ = document.getElementsByClassName("statusService");
