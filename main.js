c080f23d8d8682add3e186f5683c2f8d3f5b5306
Григорий Губарев <g.gubarev@outlook.com>
2021-03-20 02:10:24 +0400

    init4

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-﻿// async function StatusCheck() {
-//     let url = 'https://api.binance.com/wapi/v3/systemStatus.html';
-//     let response = await fetch(url);
-//     let state = await response.json(); // читаем ответ в формате JSON
-//     return console.log("state is " + state);
-// }
+﻿async function StatusCheck() {
+    let url = 'https://api.binance.com/wapi/v3/systemStatus.html';
+    let response = await fetch(url);
+    let state = await response.json(); // читаем ответ в формате JSON
+    return console.log("state is " + state.msg);
+}
 
-let request = new XMLHttpRequest();
-request.open('GET', `https://api.binance.com/wapi/v3/systemStatus.html`);
-console.log(request);
+// window.addEventListener('load', function() {
+//     let request = new XMLHttpRequest();
+//     request.open('GET', `https://api.binance.com/wapi/v3/systemStatus.html`);
+//     console.log(request);
+//
+//     let status = document.getElementById("status");
+//
+//
+//     function updateOnlineStatus(event) {
+//         var condition = navigator.onLine ? "online" : "offline";
+//
+//         status.className = condition;
+//         status.innerHTML = condition.toUpperCase();
+//
+//         log.insertAdjacentHTML("beforeend", "Event: " + event.type + "; Status: " + condition);
+//     }
+//
+//     window.addEventListener('online',  updateOnlineStatus);
+//     window.addEventListener('offline', updateOnlineStatus);
+// });
+
+
+
