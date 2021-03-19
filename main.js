384bf71e03b5e36e6c5f6b36724fc4b49db3f880
Григорий Губарев <g.gubarev@outlook.com>
2021-03-20 03:36:19 +0400

    init4

--- main.js
+++ main.js
@@ -2,29 +2,29 @@
     let url = 'https://api.binance.com/wapi/v3/systemStatus.html';
     let response = await fetch(url);
     let state = await response.json(); // читаем ответ в формате JSON
-    localStorage['Status'] = state.status;
+    // localStorage['Status'] = state.status;
     // return statusServ.textContent += state.status;
-
+    let statusServ = document.getElementById("statusService");
+    if (state.status == 0) {
+        statusServ.textContent += "online";
+    } else {
+        statusServ.textContent += "system maintenance";
+    }
         return console.log("state is " + state.msg + " status is " + state.status);
-    //
-    // let statusServ = document.getElementsByClassName("statusService");
-    // if (state.status == 0) {
-    //     statusServ.textContent += "online";
-    // } else {
-    //     statusServ.textContent += "system maintenance";
-    // }
+
+
 
 })();
-let statusServ = document.getElementById("statusService");
-
-let localState = localStorage['Status'];
-console.log(localState);
-if (localState == 0) {
-    statusServ.innerHTML = "online";
-
-} else {
-    statusServ.innerHTML = "maintenance";
-}
+// let statusServ = document.getElementById("statusService");
+//
+// let localState = localStorage['Status'];
+// console.log(localState);
+// if (localState == 0) {
+//     statusServ.innerHTML = "online";
+//
+// } else {
+//     statusServ.innerHTML = "maintenance";
+// }
 // window.addEventListener('load', function() {
 //
 //     let status = document.getElementById("status");
