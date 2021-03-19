a50f4485218d8eb6f41da6b80481ff3734c5e800
Григорий Губарев <g.gubarev@outlook.com>
2021-03-20 03:03:50 +0400

    init4

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
     let state = await response.json(); // читаем ответ в формате JSON
     let statusServ = document.getElementsByClassName("statusService1");
     // return statusServ.textContent += state.status;
-    if (state.status == "0") {
+    if (state.status == 0) {
         statusServ.textContent = "online";
     } else {
         statusServ.textContent = "maintenance";
