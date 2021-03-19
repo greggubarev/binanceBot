5ccdbd278916c49349a5bcafb0a42f2335abdb52
Григорий Губарев <g.gubarev@outlook.com>
2021-03-20 01:46:18 +0400

    init3

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-﻿
+﻿async function StatusCheck() {
+    let url = 'https://api.binance.com/wapi/v3/systemStatus.html';
+    let response = await fetch(url);
+    let state = await response.json(); // читаем ответ в формате JSON
+
+    if (state) {
+        return console.log("state is " + state);
+    } else {
+        return console.log("no state")
+    }
+}
